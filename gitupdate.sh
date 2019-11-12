5ecb2adf347d5d24f508fe49b076124bc3e6bd88
vladreamer <vladreamer@gmail.com>
2019-11-12 16:05:57 -0500

    rev.Nov12_2019_2

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-cd /home/homeassistant/.homeassistant
-source /srv/homeassistant/bin/activate
-hass --script check_config
+#hass --script check_config
+PRE_EXEC="cd /srv/homeassistant; python3 -m venv .; source bin/activate;"
 
+# Typically /usr/bin/hass
+HASS_BIN="hass"
+RUN_AS="homeassistant"
+CMD="$HASS_BIN --script check_config"
+su -s /bin/bash -c "$CMD" $RUN_AS
+
+
+
+cd /home/homeassistant/.homeassistant
 git add .
 git status
 echo -n "Enter the Description for the Change: " 
